1937d3240d28b8f5b205a9fe2c1a9ffe1b25cb0b
Yannis De Cleene <yannisdcl@gmail.com>
2014-04-21 19:20:50 +0200

    Switching on/off
    Made the on/off button switching and fancied up the UI.

--- SparkRelayControl/toggle.js
+++ SparkRelayControl/toggle.js
@@ -1,28 +1,47 @@
 $(document).ready(function(){
-  $("#on").click(function(){
-    $.ajax({
-      type: "POST",
-      url: "https://api.spark.io/v1/devices/INSERT-YOUR-DEVICE-ID/led",
-      data: {
-	    access_token: "INSERT-YOUR-ACCESS-TOKEN",
-	    params: "l1,HIGH"
-      },
-      success: function() { $("#result").text("Success"); } ,
-      dataType: "json"
-    });
-    //alert("You just turned me on!")
+    $(function(){ // DOM ready
+
+      $("#on, #off").click(function(){
+        var io = this.io ^= 1;
+        $(this).text(io?"ON":"OFF").prop('id', io?"on":"off");
+        if(io){
+          $.ajax({
+              type: "POST",
+              url: "https://api.spark.io/v1/devices/INSERT-YOUR-DEVICE-ID/led",
+              data: {
+	            access_token: "INSERT-YOUR-ACCESS-TOKEN",
+	            params: "l1,HIGH"
+              },
+             success: function() { $("#result").text("Success"); } ,
+              dataType: "json"
+            });
+          document.getElementById("on").innerHTML="<p class='but'>OFF</p>";
+          document.getElementById("on").id="off";
+        }else{
+          $.ajax({
+              type: "POST",
+              url: "https://api.spark.io/v1/devices/INSERT-YOUR-DEVICE-ID/led",
+              data: {
+	            access_token: "INSERT-YOUR-ACCESS-TOKEN",
+	            params: "l1,LOW"
+              },
+             success: function() { $("#result").text("Success"); } ,
+              dataType: "json"
+            });
+          document.getElementById("off").innerHTML="<p class='but'>ON</p>";
+          document.getElementById("off").id="on";
+        }
+      });  
+        
+    $("#button")
+  .on('click', '#test', function(){
+    $.getJSON("https://api.spark.io/v1/devices/INSERT-YOUR-DEVICE-ID/ledOne?access_token=INSERT-YOUR-ACCESS-TOKEN", function(person){
+
+    var p = JSON.parse(person.result);
+    document.getElementById("result").innerHTML=p;
+});
   });
 
-$("#off").click(function(){
-    $.ajax({
-      type: "POST",
-      url: "https://api.spark.io/v1/devices/INSERT-YOUR-DEVICE-ID/led",
-      data: {
-	    access_token: "INSERT-YOUR-ACCESS-TOKEN",
-	    params: "l1,LOW"
-      },
-      success: function() { $("#result").text("Success"); } ,
-      dataType: "json"
-    });
-  });
+    });  
+    
 });
