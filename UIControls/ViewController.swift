e2d8199e20b8d6551543f19eda1f36ae952bdcc8
Yannis De Cleene <yannisdcl@gmail.com>
2014-11-17 18:12:25 +0100

    Initial Commit

---
+++ UIControls/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIControls
+//
+//  Created by Yannis De Cleene on 17/11/14.
+//  Copyright (c) 2014 Yannis De Cleene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
